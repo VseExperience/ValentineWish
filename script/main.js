cd8b71cb323965c20d8bfbcf4cf14516f553c680
VseExperience <59097012+VseExperience@users.noreply.github.com>
2026-02-14 04:24:32 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -254,7 +254,7 @@ const animationTimeline = () => {
       opacity: 0,
       y: 30,
       zIndex: "-1",
-    }, "+=8")
+    }, "+=10")
     .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
     .to(
       ".last-smile",
